24e0d7f09c3cbdc6be488a1be4196c1a975b86d1
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2021-04-06 03:07:54 +0200

    U

--- scripts/copy-file-mac.js
+++ scripts/copy-file-mac.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
-var dir = './released';
+var dir = '/released';
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
 
-var files = fs.readdirSync('./release/').filter((fn) => fn.endsWith('.dmg'));
+var files = fs.readdirSync('/release/').filter((fn) => fn.endsWith('.dmg'));
 if (files[0]) {
-  fs.copyFile('./release/' + files[0], './released/' + files[0], (err) => {
+  fs.copyFile('/release/' + files[0], '/released/' + files[0], (err) => {
     if (err) throw err;
   });
 }
-fs.copyFile('./release/latest.yml', './released/latest.yml', (err) => {
+fs.copyFile('/release/latest.yml', '/released/latest.yml', (err) => {
   if (err) throw err;
 });
