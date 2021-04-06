3356d3f3567865c0e39587b6e4a89b7272cd9549
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2021-04-06 15:41:25 +0200

    Fixed Main TS

--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 require('@electron/remote/main').initialize();
 
-import {
-  app,
-  BrowserWindow,
-  Menu,
-  nativeImage,
-  screen,
-  Tray,
-  dialog,
-} from 'electron';
+import { app, BrowserWindow, dialog, Menu, nativeImage, Tray } from 'electron';
+import { autoUpdater } from 'electron-updater';
+
 import * as path from 'path';
 import * as url from 'url';
-import { autoUpdater } from 'electron-updater';
 
 let type: string;
 if (process.platform === 'win32') {
@@ -62,12 +55,11 @@ const gotTheLock = app.requestSingleInstanceLock();
 const args = process.argv.slice(1),
   serve = args.some((val) => val === '--serve');
 
-function createWindow(): BrowserWindow {
-  autoUpdater.checkForUpdates();
+// eslint-disable-next-line @typescript-eslint/require-await
+async function createWindow(): Promise<BrowserWindow> {
+  // autoUpdater.checkForUpdates();
   app.setAppUserModelId('de.codext.remotedesktop-control');
   app.allowRendererProcessReuse = false;
-  const size = screen.getPrimaryDisplay().workAreaSize;
-  console.log('process.platform', process.platform);
 
   // Create the browser window.
   win = new BrowserWindow({
@@ -99,19 +91,20 @@ function createWindow(): BrowserWindow {
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 */
-  const iconPath = path.join(__dirname, 'data/icon.png');
+  const iconPath = path.join(__dirname, 'data/icon-no-bg-small.png');
+
   tray = new Tray(nativeImage.createFromPath(iconPath));
   const contextMenu = Menu.buildFromTemplate([
     {
       label: 'Öffnen',
       click: () => {
-        win.show();
+        win?.show();
       },
     },
     {
       label: 'Schließen',
       click: () => {
-        win.close();
+        win?.close();
       },
     },
   ]);
@@ -119,7 +112,7 @@ function createWindow(): BrowserWindow {
   tray.setContextMenu(contextMenu);
 
   tray.on('click', () => {
-    win.show();
+    // win.show();
   });
 
   if (serve) {
@@ -145,7 +138,7 @@ function createWindow(): BrowserWindow {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    // win = null;
   });
 
   win.on('close', (e) => {
@@ -180,7 +173,10 @@ try {
     });
 
     // Create myWindow, load the rest of the app, etc...
-    app.on('ready', createWindow);
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.on('ready', async () => {
+      await createWindow();
+    });
   }
 
   app.on('activate', () => {
