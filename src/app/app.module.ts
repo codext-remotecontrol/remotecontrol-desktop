78273b66a21be9f601dec9e6bc8a945fde966c3c
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2021-08-03 00:04:50 +0200

    U

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
@@ -13,11 +13,12 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
-import { RouteReuseStrategy } from '@angular/router';
+import { Router, RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgxElectronModule } from 'ngx-electron';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Sentry from '@sentry/angular';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -54,6 +55,22 @@ export function playerFactory() {
     MatTooltipModule,
   ],
   providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: false,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,29 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { AppConfig } from './environments/environment';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
 
 if (AppConfig.production) {
+  Sentry.init({
+    dsn: 'https://70e58945567645c7be269a0b55cb1e0e@sentry.codext.de/12',
+    integrations: [
+      new Integrations.BrowserTracing({
+        tracingOrigins: ['localhost'],
+        routingInstrumentation: Sentry.routingInstrumentation,
+      }),
+    ],
+  });
   enableProdMode();
+  if (window) {
+    window.console.log = () => {};
+    window.console.warn = () => {};
+    window.console.info = () => {};
+  }
 }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
-    preserveWhitespaces: false
+    preserveWhitespaces: false,
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
