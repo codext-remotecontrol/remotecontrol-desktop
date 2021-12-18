a31867c00576a8fde68977683f0fd6784669447f
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2021-12-18 21:30:37 +0100

    U

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
@@ -13,10 +11,5 @@ module.exports = {
     outline: false,
     accessibility: false,
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 };
