c324141afa113533535380e4aa7957a5f35276a8
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2023-07-16 11:13:59 +0200

    Removed Sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 
 // NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppComponent } from './app.component';
-import { Router, RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import * as Sentry from '@sentry/angular';
-
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import {
     popperVariation,
     provideTippyConfig,
     TippyDirective,
     tooltipVariation,
 } from '@ngneat/helipopper';
+import player from 'lottie-web';
+import { LottieModule } from 'ngx-lottie';
+import { AppComponent } from './app.component';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export function playerFactory() {
     return player;
@@ -64,22 +63,6 @@ export function playerFactory() {
                 popper: popperVariation,
             },
         }),
-        {
-            provide: ErrorHandler,
-            useValue: Sentry.createErrorHandler({
-                showDialog: false,
-            }),
-        },
-        {
-            provide: Sentry.TraceService,
-            deps: [Router],
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: () => () => {},
-            deps: [Sentry.TraceService],
-            multi: true,
-        },
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
         { provide: LocationStrategy, useClass: HashLocationStrategy },
     ],
